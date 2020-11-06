bb353c9077d41cbf556add1ecf8829d8818e9e95
Sandeep Verma <sandeepverma4321@gmail.com>
2020-11-06 15:44:30 +0530

    #1 Initial project commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AddRestaurantComponent } from './add-restaurant/add-restaurant.component';
+import { UpdateRestaurantComponent } from './update-restaurant/update-restaurant.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ListRestaurantComponent } from './list-restaurant/list-restaurant.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddRestaurantComponent,
+    UpdateRestaurantComponent,
+    LoginComponent,
+    RegisterComponent,
+    ListRestaurantComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
